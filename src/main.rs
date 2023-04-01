530d44ddbf855831a74051ea447787ec61db11bc
NassimRayZ <jr_zorgani@esi.dz>
2023-04-01 10:57:51 +0100

    Done: adding up a subscriber to our database.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use zero2prod::config::get_config;
 use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_config().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connection to Postgres.");
     let socket = format!("localhost:{}", config.application_port);
     let listener = TcpListener::bind(socket)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
