11dae2c516f094d01a5843324ef5e848c7c8864b
NassimRayZ <jr_zorgani@esi.dz>
2023-08-12 02:03:10 +0100

    Edit: added first steps of newsletter delivery "/newsletters route"

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, subscribe};
+use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 
 use std::net::TcpListener;
 
@@ -76,6 +76,7 @@ pub fn run(
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
---
+++ tests/api/newsletter.rs
@@ -0,0 +1,119 @@
+use wiremock::matchers::{any, method, path};
+use wiremock::{Mock, ResponseTemplate};
+
+use crate::helpers::{spawn_app, ConfirmationLinks, TestApp};
+
+#[tokio::test]
+async fn newsletters_not_delivered_to_unconfirmed_subscribers() {
+    let test_app = spawn_app().await;
+    create_unconfirmed_subscriber(&test_app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&test_app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+
+    let response = test_app.post_newsletters(newsletter_request_body).await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn newsletters_delivered_to_confirmed_subscribers() {
+    let test_app = spawn_app().await;
+    create_confirmed_subscriber(&test_app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&test_app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+    let response = test_app.post_newsletters(newsletter_request_body).await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+async fn create_unconfirmed_subscriber(test_app: &TestApp) -> ConfirmationLinks {
+    let body = "name=rayene%20nassim&email=jr_zorgani%40esi.dz";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber.")
+        .expect(1)
+        .mount_as_scoped(&test_app.email_server)
+        .await;
+
+    test_app
+        .post_subscriptions(body)
+        .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &test_app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    test_app.get_confirmation_links(email_request)
+}
+
+async fn create_confirmed_subscriber(test_app: &TestApp) {
+    let confirmation_links = create_unconfirmed_subscriber(test_app).await;
+    reqwest::get(confirmation_links.plain_text)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+}
+
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    let test_app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+            }),
+            "missing title",
+        ),
+        (
+            serde_json::json!({"title": "Newsletter!"}),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = test_app.post_newsletters(invalid_body).await;
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
