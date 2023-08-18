5fe2af880237b5fd9f5c1dbdbc8be928ae51348c
NassimRayZ <jr_zorgani@esi.dz>
2023-08-18 23:40:41 +0100

    Edit: added logout functionality

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -8,3 +8,23 @@ async fn must_be_logged_in_to_access_admin_dashboard() {
 
     assert_is_redirect_to(&response, "/login");
 }
+
+#[tokio::test]
+async fn logout_clears_session_state() {
+    let test_app = spawn_app().await;
+
+    let response = test_app.post_login_test_user().await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    let html_page = test_app.get_admin_dashboard_html().await;
+    assert!(html_page.contains(&format!("Welcome {}", test_app.test_user.username)));
+
+    let response = test_app.post_logout().await;
+    assert_is_redirect_to(&response, "/login");
+
+    let html_page = test_app.get_login_html().await;
+    assert!(html_page.contains("You have successfully logged out."));
+
+    let response = test_app.get_admin_dashboard().await;
+    assert_is_redirect_to(&response, "/login");
+}
